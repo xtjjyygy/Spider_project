a2a9d712cf35b23d9e7052c25f79b6db152560ba
xtjjyygy <38133593+xtjjyygy@users.noreply.github.com>
2019-05-03 13:24:18 +0800

    Add files via upload

---
+++ scrapy/zhuanti/zhuanti/spiders/zhuantispider.py
@@ -0,0 +1,32 @@
+import scrapy
+from scrapy.spiders import CrawlSpider
+from scrapy.selector import Selector
+from scrapy.http import Request
+from zhuanti.items import ZhuantiItem
+
+class zhuanti(CrawlSpider):
+    name = 'zhuanti'
+    start_urls = ['http://www.jianshu.com/recommendations/collections?page=1&order_by=hot']
+
+    def parse(self, response):
+        item = ZhuantiItem()
+        selector = Selector(response)
+        infos = selector.xpath('//div[@class="col-xs-8"]')
+        for info in infos:
+            try:
+                name = info.xpath('div/a/h4/text()').extract()
+                content = info.xpath('div/a[1]/p/text()').extract()
+                article = info.xpath('div/div/a/text()').extract()
+                fans = info.xpath('div/div/text()').extract()
+
+                item['name'] = name
+                item['content'] = content
+                item['article'] = article
+                item['fans'] = fans
+
+                yield item
+            except IndexError:
+                pass
+        urls = ['http://www.jianshu.com/recommendations/collections?page={}&order_by=hot'.format(str(i)) for i in range(2,39)]
+        for url in urls:
+            yield Request(url,callback=self.parse)
